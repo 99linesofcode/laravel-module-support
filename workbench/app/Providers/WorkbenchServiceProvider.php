4737dd0d78fdfe2f01b778ddff3b478f07dade43
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-03-19 17:50:31 +0100

    fix(livewire): loading layouts, pages and components from workbench path

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -4,6 +4,8 @@ namespace Workbench\App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use function Orchestra\Testbench\workbench_path;
+
 class WorkbenchServiceProvider extends ServiceProvider
 {
     /**
@@ -11,6 +13,17 @@ class WorkbenchServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // Livewire is configured to resolve it's layout and components from the
+        // default layouts and pages directory but it points to the incorrect
+        // directory when developing with workbench. Let us override that.
+        config()->set('livewire', [
+            'component_layout' => 'workbench::layouts.app',
+            'component_namespaces' => [
+                'layouts' => workbench_path('resources/views/layouts'),
+                'pages' => workbench_path('resources/views/pages'),
+            ],
+        ]);
+
         $this->app->register(AdminPanelProvider::class);
     }
 
